12cac8a71ab8f0a4eb3206d6b56eb58e7be52e77
Saurabh Patondikar <33091445+saurabhpatondikar@users.noreply.github.com>
2019-06-22 17:16:21 +0530

    React Router

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,19 +1,18 @@
 import React,{Component} from 'react';
-
+import {BrowserRouter} from 'react-router-dom';
 import{Navbar,NavbarBrand} from 'reactstrap';
 import Main from './components/MainComponent';
 import './App.css';
 import{DISHES} from'./shared/dishes';
 
 class App extends Component{
-  
-
-
 render() {
   return (
+    <BrowserRouter>
     <div>
      <Main/>
     </div>
+    </BrowserRouter>
   );
  }
 }
